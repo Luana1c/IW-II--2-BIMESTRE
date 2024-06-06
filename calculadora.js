9f636e07c5881d3a799fd3d6bc6fd3c817203b51
Luana1c <luanagm09@gmail.com>
2024-06-06 07:58:24 -0300

    novo

--- calculadora.js
+++
@@ -1,9 +0,0 @@
-var SomarFunc = require("./Modulos/somar")
-var SubtrairFunc = require("./Modulos/subtrair")
-var MultiplicarFunc = require("./Modulos/multiplicar")
-var DividirFunc = require("./Modulos/dividir")
-
-console.log("A soma é" , SomarFunc(10,8))
-console.log("A subtração é" , SubtrairFunc(10,8))
-console.log("A multiplicação é" , MultiplicarFunc(10,8))
-console.log("A divisão é" , DividirFunc(10,8))
